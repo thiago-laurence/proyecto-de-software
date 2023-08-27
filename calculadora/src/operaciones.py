3a2e7fb988ec9880de47958d4661454406d0a2e3
Juana Borrelli Zara <jborrellizara@gmail.com>
2023-08-27 15:33:42 -0300

    feat: agrego operaciones de la calculadora

---
+++ calculadora/src/operaciones.py
@@ -0,0 +1,15 @@
+def suma(num1,num2):
+    return num1 + num2
+
+def resta(num1,num2):
+    return num1 - num2
+
+def multiplicacion(num1,num2):
+    return num1 * num2
+
+def division(num1,num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: No se puede dividir por cero"
+    
