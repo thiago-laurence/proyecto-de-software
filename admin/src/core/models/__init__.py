2e81cfe483f85dc7f57e7064bbbaf902080ddff7
Facundo Aguel <facundoaguel@gmail.com>
2023-09-20 15:36:58 -0300

    STYLE: agrego docstrings

--- admin/src/core/models/__init__.py
+++ admin/src/core/models/__init__.py
@@ -1,10 +1,17 @@
 from src.core.models.user import User
 from src.core.database import db
+
 def list_all_users():
+    """
+    Listar todos los usuarios de la base de datos.
+    """
     users = User.query.all()
     return users
 
 def create_user(**kwargs):
+    """"
+    Crear un usuario y almacenarlo en la db.
+    """
     user = User(**kwargs)
     db.session.add(user)
     db.session.commit()
