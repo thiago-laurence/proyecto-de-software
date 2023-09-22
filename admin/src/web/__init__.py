2e03101cf5d6d182bf2720e6ada7e5a79a06cf30
Juana Borrelli Zara <jborrellizara@gmail.com>
2023-09-22 19:52:47 -0300

    feat(Modulo Usuarios): Inicio del modulo de usuarios

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import render_template
 
-from src.web.config import config
-
 from src.core import models
 from src.core import database
+from src.core import seeds
+
+from src.core.models.users.user import User
 
 from src.web import error
+from src.web.config import config
+from src.web.controllers.users import users_blueprint
 
 
 def create_app(env="development", static_folder="../../static"):
@@ -23,10 +26,17 @@ def create_app(env="development", static_folder="../../static"):
         return render_template("home.html")
 
     app.register_error_handler(404, error.not_found_error)
+    
+    #Blueprints
+    app.register_blueprint(users_blueprint)
 
     #Comandos
     @app.cli.command(name="resetdb")
     def resetdb():
         database.reset_db()
+        
+    @app.cli.command(name="seedsdb")
+    def seedsdb():
+        seeds.run()
 
     return app
