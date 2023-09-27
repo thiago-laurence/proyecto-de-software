fd3abbcd8e2316913e0f5b2f483b78b5ac1386f1
Juana Borrelli Zara <jborrellizara@gmail.com>
2023-09-27 14:25:13 -0300

    feat(ModuloServicios): hecho el crud basico de los servicios

--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -1,6 +1,7 @@
 from src.web.controllers.users.users import users_blueprint
 from src.web.controllers.authentication.auth import auth_blueprint
 from src.web.controllers.institutions.institutions import institutions_blueprint
+from src.web.controllers.institutions.services import services_blueprint
 from src.web.controllers.home.home import home_blueprint
 from src.web.controllers.api import api_blueprint
 
@@ -12,4 +13,5 @@ def register_blueprints(app):
     app.register_blueprint(users_blueprint)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(institutions_blueprint)
+    app.register_blueprint(services_blueprint)
     app.register_blueprint(api_blueprint)
