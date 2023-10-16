3dcb5c1f1b86ac02647a18cd9091b31fa93d60c5
thiago-laurence <francolaurence47@gmail.com>
2023-10-16 16:26:23 -0300

    fix(api-users): se agrego api de usuarios en blueprint de apis

--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -1,6 +1,5 @@
 from src.web.controllers.system.system import system_blueprint
 from src.web.controllers.users.users import users_blueprint
-from src.web.api.users import api_users
 from src.web.controllers.authentication.auth import auth_blueprint
 from src.web.controllers.institutions.institutions import institutions_blueprint
 from src.web.controllers.institutions.services import services_blueprint
@@ -16,7 +15,6 @@ def register_blueprints(app):
     app.register_blueprint(system_blueprint)
     app.register_blueprint(home_blueprint)
     app.register_blueprint(users_blueprint)
-    app.register_blueprint(api_users)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(institutions_blueprint)
     app.register_blueprint(iu_blueprint)
