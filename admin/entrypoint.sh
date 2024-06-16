b8b0d627f2395588d0314ee5687b01b08b86c440
Thiago Laurence <francolaurence47@gmail.com>
2024-06-16 14:19:46 -0300

    docker(compose): actualizacion de docker-compose para utlizar bind-mounts sobre el codigo del proyecto Admin

--- admin/entrypoint.sh
+++ admin/entrypoint.sh
@@ -2,8 +2,18 @@
 
 echo "Starting entrypoint.sh..."
 
+# Instalando dependencias
+apk add --no-cache gcc musl-dev libffi-dev openssl-dev
+
+# Instalando poetry
+pip install poetry==1.6.1
+
+# Instalando dependencias del proyecto
+poetry install
+
 # Creando base de datos
 poetry run flask resetdb
+
 # Instanciando datos iniciales
 poetry run flask seedsdb
 
